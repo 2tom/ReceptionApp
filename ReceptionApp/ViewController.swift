d2aa99606492fcad7b567b3c58603f33fde4444b
Tsutomu Teranishi <tsutomu@tsutomunomacbook-pro.local>
2019-02-13 10:09:17 +0900

    Initial Commit

---
+++ ReceptionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReceptionApp
+//
+//  Created by Tsutomu Teranishi on 2019/02/13.
+//  Copyright © 2019 Tsutomu Teranishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
